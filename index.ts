5f5c1f46793485bc41cc013c28776e37e52dcbe6
An72ty <an72ty@gmail.com>
2026-01-18 21:42:33 +0500

    commit 2

--- index.ts
+++ index.ts
@@ -80,12 +80,108 @@ interface IThirdDimCoordinates extends Coordinates {
 }
 
 // Task 3.1
-// function printLength(x: string | string[]): number {
-//     if (typeof x === "string") {
-//         return x.length;
-//     } else if (Array.isArray(x)) {
-//         return x.length;
-//     }
+function printLength(x: string | string[]): void {
+    if (typeof x === "string") {
+        console.log(x.length);
+    } else if (Array.isArray(x)) {
+        console.log(x.length);
+    }
+} 
 
-//     return x.length;
-// } ! Не сделано
+// Task 3.2
+let pageNumber: unknown = "123";
+console.log(pageNumber as number + 1); // Значение воспринимается как число, но переменная все еще остается строкой
+console.log(Number(pageNumber) + 1); // Здесь происходит прямое приведение к типу, из числа в строку
+
+// Task 3.3
+interface Car {
+    engine: string,
+    wheels: number
+}
+
+function isCar(obj: any): boolean {
+    if ("engine" in obj && "wheels" in obj) {
+        return true;
+    } else return false;
+}
+
+// Task 4.1
+function getFirstElement<T>(x: T[]): T {
+    return x[0];
+}
+
+console.log(getFirstElement(["asdfasd", "asdf", "sadfasdf", "sdaf"]));
+console.log(getFirstElement([1, 2, 3, 4, 5, 6]));
+
+// Task 4.2
+type HasLength = {
+    length: number
+}
+
+function logLength<T extends HasLength>(obj: T): number {
+    return obj.length;
+}
+
+// Task 4.3
+interface ApiResponse<T> {
+    data: T,
+    success: boolean,
+    message?: string
+}
+
+let userInfoResponse: ApiResponse<User> = {
+    data: {
+        id: 123456,
+        name: "user123",
+        email: "example@mail.com"
+    },
+    success: true,
+    message: "200"
+}
+
+// Task 5.1
+type BaseUser = {
+    email: string,
+    password: string
+}
+
+type UserProfile = {
+    firstName: string,
+    lastName: string,
+    age: number
+}
+
+type DraftUserProfile = Partial<UserProfile>;
+type ServerSendUser = Required<UserProfile & BaseUser>
+type SiteUserProfile = Pick<UserProfile, "firstName"> & Omit<BaseUser, "password">
+
+// Task 5.2
+interface Product<T extends string | number> {
+    id: T,  
+    name: string,
+    price: number
+}
+
+type CartItem<T extends string | number> = Product<T> & { quantity: number }
+
+function calculateTotal<T extends string | number>(items: CartItem<T>[]): number {
+    let totalSum = 0;
+    items.forEach(x => totalSum += x.quantity * x.price);
+    return totalSum;
+}
+
+// Task 5.3
+function validateInput(value: unknown): value is string {
+    if (typeof value === "string" && value.trim()) {
+        return true;
+    }
+    return false;
+}
+
+let testString: string = "  test string   ";
+let testString2: string = "  ";
+let notString: number = 123;
+
+if (validateInput(testString)) console.log(testString.toUpperCase());
+if (validateInput(testString2)) console.log(testString2.toUpperCase());
+if (validateInput(notString)) console.log("123".toUpperCase());
